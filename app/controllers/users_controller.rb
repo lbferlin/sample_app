e403e168295d9104ef82e19e3dbf80d59c8def61
Leandro Ferlin <leandro.ferlin@resultadosdigitais.com.br>
2011-09-09 19:38:54 -0300

    Stopped at Figure 8.6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,18 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
-  def new
+   def new
+    @user = User.new
     @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
