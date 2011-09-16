6577f053b48a8ea59ebd1387d35c53c25853aa0d
Leandro Ferlin <leandro.ferlin@resultadosdigitais.com.br>
2011-09-16 19:32:14 -0300

    Stopped at Listing 9.22

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
     @title = "Sign in"
   end
 
-  def create
-     render 'new'
+ def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      sign_in user
+      redirect_to user    end
   end
 
-  def destroy
+    def destroy
+    sign_out
+    redirect_to root_path
   end
 end
